013662ad7149b71b78aeaf18f5046a82960e2079
Rene Gudmundsson <mikum777@gmail.com>
2024-05-10 19:24:06 -0400

    Se configuro la capa dao

---
+++ src/main/java/com/gudmundsson/subscription/dao/MItemServiceMapper.java
@@ -0,0 +1,5 @@
+package com.gudmundsson.subscription.dao;
+
+public interface MItemServiceMapper {
+
+}
---
+++ src/main/java/com/gudmundsson/subscription/dao/MUserMapper.java
@@ -0,0 +1,8 @@
+package com.gudmundsson.subscription.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MUserMapper {
+
+}
